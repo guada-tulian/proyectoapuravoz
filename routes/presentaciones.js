da419acc6d2c740a9e45bd50dcccfc5ae7222ef0
guadalupe <guada2tulian@gmail.com>
2021-12-28 23:53:38 -0300

    controls

--- routes/presentaciones.js
+++ routes/presentaciones.js
@@ -1,8 +1,10 @@
 var express = require('express');
 var router = express.Router();
 
-router.get('/', function (req,res,next) {
-    res.render('presentaciones')
-})
+router.get('/', (req, res, next) => {
+        res.render('presentaciones', {
+            isPresentaciones: true
+        });
+    })
 
 module.exports = router;
