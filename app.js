f8c8ba802a5689baa0138ec6e42ee7a0a05795de
guadalupe <guada2tulian@gmail.com>
2021-12-29 23:54:28 -0300

    nuevos cambios

--- app.js
+++ app.js
@@ -5,13 +5,16 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
 require('dotenv').config();
+var session = require('express-session');
 
 var indexRouter = require('./routes/index');
 var novedadesRouter = require('./routes/novedades');
 var galeriaRouter = require('./routes/galeria');
 var presentacionesRouter = require('./routes/presentaciones');
-var contactoRouter = require('./routes/contacto');
-var loginRouter =require('./routes/admin/login');
+var contactoRouter =require('./routes/contacto');
+var loginRouter = require('./routes/admin/login');
+var adminNovedadesRouter = require('./routes/admin/novedades');
+
 var app = express();
 
 // view engine setup
@@ -24,13 +27,20 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.use(session({
+  secret: 'Hola123467890',
+  resave: false,
+  saveUnitialized: true,
+  cookie: {secure: true}
+})
+
 app.use('/', indexRouter);
 app.use('/novedades', novedadesRouter);
 app.use('/galeria', galeriaRouter);
 app.use('/presentaciones', presentacionesRouter);
 app.use('/contacto', contactoRouter);
 app.use('/admin/login', loginRouter);
-
+app.use('/admin/novedades', adminNovedadesRouter);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
